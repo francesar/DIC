96bab391f0ba2b2ca61fe6d71012f6f43457da17
Yulissa Arroyo-Paredes <yulissa@dyn-160-39-182-59.dyn.columbia.edu>
2018-02-17 20:15:33 -0500

    Updated the AST to include data types specific to our lang

--- ast.ml
+++ ast.ml
@@ -3,30 +3,37 @@
 (* Operators *)
 (* For now, matrix ops have *_M prefix to denote ops on matrices *)
 type op = Add | Sub | Mult | Div | Equal | Neq | Less |
-          Leq | Greater | Geq | And | Or | Mod | CmpStruct |
-          CmpPhys |
-          Add_M | Sub_M | Mult_M | Dot_M | 
+          Leq | Greater | Geq | And | Or | Mod | 
+          CmpStruct | CmpPhys |
+          Add_M | Sub_M | Mult_M | Dot_M | Div_M
 
 type uop = Neg | Not | Trans_M
 
 (* Primitive Types *)
 type typ = Int | Bool | Char | Float | Null
 
+(* Data Types *)
+type datatyp = String | List | Dict | Matrix
+
 type bind = typ * string
 
 type expr = 
-  | Literal of int 
+    Literal of int 
   | Fliteral of string 
   | BoolLit of bool
+  | StrLit of string
+  | MatLit of (expr list list)
   | Id of string 
   | Binop of expr * op * exp
   | Unop of uop * expr
   | Assign of string * expr
   | Call of string * expr list
+  | MatIndex of (string * expr * expr)
+  | MatIndexAssign of (string * expr * expr * expr)
   | Noexpr
 
 type stmt = 
-  | Block of stmt list 
+    Block of stmt list 
   | Expr of expr 
   | Return of expr 
   | If of expr * stmt * expr 
