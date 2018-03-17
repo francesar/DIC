6b2a82e5cb34071f2d6080eaed6510da5ecc6458
Yulissa Arroyo-Paredes <y18arroyo@gmail.com>
2018-03-17 00:51:10 -0400

    just added Sast line to toplevel.ml

--- toplevel.ml
+++ toplevel.ml
@@ -4,4 +4,5 @@ let () =
   Arg.parse [] (fun file -> channel := open_in file) usage_msg;
   let lexbuf = Lexing.from_channel !channel in
   let ast = Parser.program Scanner.token lexbuf in
-  print_string (Ast.string_of_program ast)
+  let sast = Semant.check ast in 
+  print_string (Sast.string_of_sprogram sast)
