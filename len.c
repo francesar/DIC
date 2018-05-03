4c2c6e30a02d92d8d9acc5421e9d3612b0707113
cesaribarra10 <cfi2103@columbia.edu>
2018-05-03 05:40:56 -0400

    adding initial code for list append

--- len.c
+++ len.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
-#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
 
 typedef struct int_array {
 	int length;
@@ -9,5 +10,28 @@ typedef struct int_array {
 int len(void *a) {
 	struct int_array *inp_arr = *(int_array**)(a);
 	int size = inp_arr->length;
+
     return size;
+}
+
+int_array* append(void *a, int *new_element) {
+    struct int_array *passed_in_arr = *(int_array**)(a);
+    struct int_array new_struct;
+
+    int old_size = passed_in_arr->length;
+
+    new_struct.length = old_size + 1;
+
+    for(int i = 0; i < old_size; i++) {
+        int val = *(passed_in_arr->arr);
+        new_struct.arr = &val;
+
+        new_struct.arr++;
+        passed_in_arr->arr++;
+    }
+
+    int new_element = 1;
+    new_struct.arr = &new_element;
+
+    return &new_struct;
 }
